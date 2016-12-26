ca17c1a62b05a85df0e14945cf8c924b871c68c7
Vutukuri Sai Siva Krishna <sskrishnavutukuri@gmail.com>
2016-12-26 21:55:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='My_Python_Apps', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['flask', 'PIL', 'wkhtmltopdf', 'Image', 'python-crontab']
+      install_requires=['flask', 'Pillow', 'wkhtmltopdf', 'Image', 'python-crontab']
      )
